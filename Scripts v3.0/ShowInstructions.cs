464c7d41fb89c524c71cb797ac905539e9de673c
Kate Tyshchenko <54598537+ktyshchenko@users.noreply.github.com>
2021-04-24 16:06:28 -0700

    Add hover over instr for destructor

---
+++ Scripts v3.0/ShowInstructions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowInstructions : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject instr;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        instr.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        instr.SetActive(false);
+    }
+}
